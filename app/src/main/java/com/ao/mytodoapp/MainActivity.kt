b7ac5fbf9a8b230f37cd257ec48cbfdd966fb10d
ahmet-ozberk <ahmet.ozberk.mrs@gmail.com>
2023-11-15 16:10:33 +0300

    Fix

--- app/src/main/java/com/ao/mytodoapp/MainActivity.kt
+++ app/src/main/java/com/ao/mytodoapp/MainActivity.kt
@@ -9,7 +9,6 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
